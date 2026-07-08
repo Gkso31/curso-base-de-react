f500e3905b355f6dcd40eba66098819de78f1f9d
Gustavo Ota <gshiraha@jbssa.com>
2026-07-08 11:00:38 -0300

    Finishing first part of the React's course

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,59 @@
+import { useState } from "react";
+
 export function App() {
+  const [list, setList] = useState([
+    { id: 1, label: "Tomate", complete: false },
+    { id: 2, label: "Laranja", complete: false },
+    { id: 3, label: "Arroz", complete: false },
+    { id: 4, label: "Manga", complete: false },
+  ]);
+  const [valor, setValor] = useState("");
 
   return (
-    <>
-      Olá
-    </>
-  )
-}
+    <div>
+      <input value={valor} onChange={(v) => setValor(v.target.value)} />
 
+      <button
+        onClick={() => {
+          setList([...list, { id: list.length + 1, label: valor, complete: false }]);
+          setValor('');
+        }}
+      >
+        Adicionar
+      </button>
+
+      <ol>
+        {list.map((listItem) => (
+          <li key={listItem.id}>
+            {listItem.label}
+
+            {listItem.complete ? 'Concluído' : ''}
+
+            <button
+              onClick={() => {
+                setList([
+                  ...list.map(item => ({
+                    ...item,
+                    complete: item.id === listItem.id ? true : item.complete
+                  }))
+                ]);
+              }}
+            >
+              Concluir
+            </button>
+
+            <button
+              onClick={() => {
+                setList(() => [
+                  ...list.filter((item) => item.id !== listItem.id),
+                ]);
+              }}
+            >
+              Remover
+            </button>
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
+}
